7bfc80939cb62c0fe2e9ac5e67ba231cfdce85e9
infi-yoyo <adarsh@goyoyo.ai>
2025-09-17 15:23:11 +0530

    Update automated_mail_BS.py
    enabled for testing credential to test mail body changes

--- scripts/automated_mail_BS.py
+++ scripts/automated_mail_BS.py
@@ -97,11 +97,11 @@ def service_gmail_api():
 
 service = service_gmail_api()
 
-cc_emails = ["prakhar@goyoyo.ai", "nikhil@goyoyo.ai", "harshal@goyoyo.ai"]
-#cc_emails = []
+#cc_emails = ["prakhar@goyoyo.ai", "nikhil@goyoyo.ai", "harshal@goyoyo.ai"]
+cc_emails = []
 
-to_emails = ["mudita.gupta@bluestone.com"]
-#to_emails = ['adarsh@goyoyo.ai']
+#to_emails = ["mudita.gupta@bluestone.com"]
+to_emails = ['adarsh@goyoyo.ai']
 
 def create_html_message(sender, to, subject, html_content, cc_emails):
     """Create a message with HTML content for Gmail API."""

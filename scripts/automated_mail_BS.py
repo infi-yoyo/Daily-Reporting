c4e46e09197077cc880ab60bc31665b5ca45463c
adarsh <adarsh@goyoyo.ai>
2025-12-04 22:41:31 +0530

    Update automated_mail_BS.py
    added library

--- scripts/automated_mail_BS.py
+++ scripts/automated_mail_BS.py
@@ -10,6 +10,7 @@ from typing import Optional
 import numpy as np
 import pandas as pd
 from jinja2 import Template
+import math
 
 # Google API Libraries
 from google.auth.transport.requests import Request
